cd165ec76c914b642d65d0084b425ac3b37f8018
SHUBHAM RAVAT <7sravat@gmail.com>
2025-10-26 15:35:17 +0000

    -

--- a1_pipelined_aes/src/SubBytes.v
+++ a1_pipelined_aes/src/SubBytes.v
@@ -1,12 +1,14 @@
-module SubBytes(oriBytes, subBytes);
-	input [127:0] oriBytes; // Original input bytes
-	output wire [127:0] subBytes; // Corresponding sub_bytes 
-
-	genvar i;
-	generate 
-		for (i=7;i<128;i=i+8) begin: SubTableLoop
-			SubTable s(oriBytes[i -:8],subBytes[i -:8]);
-		end
-	endgenerate
+module SubBytes(
+    input  [127:0] oriBytes,   // Original input bytes
+    output [127:0] subBytes    // Corresponding SubBytes output
+);
+    genvar i;
+    generate
+        for (i = 0; i < 128; i = i + 8) begin : SubTableLoop
+            SubTable s (
+                .oriByte(oriBytes[i +: 8]),
+                .subByte(subBytes[i +: 8])
+            );
+        end
+    endgenerate
 endmodule
-
